7d7dc22eaab43d22f239f9a876608599364454e1
Hadi2468 <dr.hadihosseini@gmail.com>
2026-06-09 13:26:46 -0700

    best run

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DATA_PATH = {
 
 MODEL_CONFIG = {
     "model_name": "meta-llama/Llama-3.2-1B",
-    "max_seq_length": 512,
+    "max_seq_length": 2048,
     "load_in_4bit": False # LoRA: False, QLoRA: True
 }
 
@@ -20,18 +20,18 @@ LORA_CONFIG = {
 TRAINING_CONFIG = {
     "batch_size": 4,
     "gradient_accumulation_steps": 4,
-    "epochs": 20,
+    "epochs": 30,
     "learning_rate": 2e-4,
     "weight_decay": 0.01,
     "warmup_steps": 10,
     "logging_steps": 5,
     "optim": "adamw_8bit",
-    "max_seq_length": 512,
+    "max_seq_length": 2048,
 }
 
 TEST_CONFIG = {
     "max_new_tokens": 100,
-    "temperature": 0.9,
+    "temperature": 0.1,
     "top_p": 0.9,
     "repetition_penalty": 1.4,
 }
