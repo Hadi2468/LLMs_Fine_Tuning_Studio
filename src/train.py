2a3642e1c62fb139c6b33492a9c2bb2626bde4a0
Hadi2468 <dr.hadihosseini@gmail.com>
2026-06-12 18:13:55 -0700

    fix output logging issue

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import torch
+from pathlib import Path
 
 from src.model_loader import load_model
 from src.dataset_loader import load_dataset, format_dataset
@@ -28,13 +29,15 @@ def train_model(config: dict):
         "epochs",
         "learning_rate",
         "optim",
+        "job_id",
+        "dataset_path",
     ]
 
     for k in required_keys:
         if k not in config:
             raise ValueError(f"Missing config key: {k}")
         
-    print("\n======== 🚀 Starting training... ========")
+    print("\n--=>--=>--=>--=> 🚀 Starting training... --=>--=>--=>--=>")
     print(f"Model: {config['model_name']}")
     
     # Load model and tokenizer
@@ -54,13 +57,21 @@ def train_model(config: dict):
     data_path = config.get("dataset_path", DATA_PATH["data_dir"] / "train_data.json")
     dataset = load_dataset(data_path)
     formatted_dataset = format_dataset(dataset, tokenizer)
-    output_dir = str(DATA_PATH["output_dir"])
-    model_path = str(DATA_PATH["model_dir"])
-    print(f"\n======== Dataset size: {len(formatted_dataset)} ========\n")
+
+    # Create output path
+    print(f"\n--------- Dataset size: {len(formatted_dataset)} ---------\n")
+    job_id = config["job_id"]
+    gdrive_root = Path("/content/drive/MyDrive/LLMs_studio")
+    output_dir = (gdrive_root / "logs" / job_id)
+    model_path = (gdrive_root / "models" / job_id)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    model_path.mkdir(parents=True, exist_ok=True)
+    print("📁 Output:", output_dir)
+    print("📁 Model :", model_path)
 
     # Training configuration
     training_args = SFTConfig(
-        output_dir=output_dir,
+        output_dir=str(output_dir),
         per_device_train_batch_size=config["batch_size"],
         gradient_accumulation_steps=config["gradient_accumulation_steps"],
         num_train_epochs=config["epochs"],
@@ -70,7 +81,7 @@ def train_model(config: dict):
         fp16=not torch.cuda.is_bf16_supported(),
         bf16=torch.cuda.is_bf16_supported(),
         logging_steps=config.get("logging_steps", 5),
-        save_strategy="epoch",
+        save_strategy="no",         # "epoch"
         report_to="none",
         optim=config["optim"],
         dataset_text_field="text",
@@ -86,33 +97,14 @@ def train_model(config: dict):
         train_dataset=formatted_dataset,
         args=training_args,
     )
-    print("\n======== Trainer started ... ========\n")
+    print("\n>>>>>>> Trainer started ... >>>>>> \n")
 
     # Train the fine-tuned model
     trainer.train()
 
     # Save the fine-tuned model
-    model.save_pretrained(model_path)
-    tokenizer.save_pretrained(model_path)
+    model.save_pretrained(str(model_path))
+    tokenizer.save_pretrained(str(model_path))
     print("\n======== ✅ Training completed and model saved! ========\n")
     
     return True
-
-# # For Colab
-# # ---------------------------------------------------
-# if __name__ == "__main__":
-#     config = {
-#         "model_name": "meta-llama/Llama-3.2-1B",
-#         "max_seq_length": 512,
-#         "load_in_4bit": False,
-#         "r": 16,
-#         "lora_alpha": 16,
-#         "lora_dropout": 0.05,
-#         "batch_size": 4,
-#         "gradient_accumulation_steps": 4,
-#         "epochs": 1,
-#         "learning_rate": 2e-4,
-#         "optim": "adamw_8bit",
-#         "use_gpu": True,
-#     }
-#     train_model(config)
